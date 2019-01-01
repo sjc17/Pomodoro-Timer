ed5cc5097cefde52173c0381440fb534ba510cdb
Steven Choi <sjc@ualberta.ca>
2019-01-01 16:48:17 -0700

    Responsive blocks and prevent text selection

--- script.js
+++ script.js
@@ -9,87 +9,93 @@ let timerRunning = false; // 'true' for counting down, 'false' for stopped
 let completedPomodoros = 0; // How many pomodoros have been completed
 let pomoTimeSpent = 0; // Total time spent on task
 let totalTimeSpent = 0; // Total time spent on task+breaks
-let audio = document.getElementsByTagName('audio')[0];
+let audio = document.getElementsByTagName("audio")[0];
 
-document.getElementById('breakTime').innerHTML = breakTime;
-document.getElementById('sessionTime').innerHTML = sessionTime;
-document.getElementById('longBreakTime').innerHTML = longBreakTime;
-document.getElementById('timerDisplay').innerHTML = timerMinutes;
-document.getElementById('stateDisplay').innerHTML = 'Session<br/>0 pomodoros completed';
+document.getElementById("breakTime").innerHTML = breakTime;
+document.getElementById("sessionTime").innerHTML = sessionTime;
+document.getElementById("longBreakTime").innerHTML = longBreakTime;
+document.getElementById("timerDisplay").innerHTML = timerMinutes;
+document.getElementById("stateDisplay").innerHTML =
+  "Session<br/>0 pomodoros completed";
 
-document.getElementById('sessionUp').addEventListener('click', () => {
-  sessionTime++;  
-  document.getElementById('sessionTime').innerHTML = sessionTime;
+document.getElementById("sessionUp").addEventListener("click", () => {
+  sessionTime++;
+  document.getElementById("sessionTime").innerHTML = sessionTime;
   if (pomodoroState === 0) {
     timerReset();
   }
 });
-document.getElementById('sessionDown').addEventListener('click', () => {
+document.getElementById("sessionDown").addEventListener("click", () => {
   if (sessionTime > 1) {
     sessionTime--;
-  }    
-  document.getElementById('sessionTime').innerHTML = sessionTime;
+  }
+  document.getElementById("sessionTime").innerHTML = sessionTime;
   if (pomodoroState === 0) {
     timerReset();
   }
 });
-document.getElementById('breakUp').addEventListener('click', () => {
-  breakTime++;  
-  document.getElementById('breakTime').innerHTML = breakTime;
+document.getElementById("breakUp").addEventListener("click", () => {
+  breakTime++;
+  document.getElementById("breakTime").innerHTML = breakTime;
   if (pomodoroState === 1) {
     timerReset();
   }
 });
-document.getElementById('breakDown').addEventListener('click', () => {
+document.getElementById("breakDown").addEventListener("click", () => {
   if (breakTime > 1) {
-    breakTime--;  
+    breakTime--;
   }
-  document.getElementById('breakTime').innerHTML = breakTime;
+  document.getElementById("breakTime").innerHTML = breakTime;
   if (pomodoroState === 1) {
     timerReset();
   }
 });
-document.getElementById('longBreakUp').addEventListener('click', () => {
-  longBreakTime++;  
-  document.getElementById('longBreakTime').innerHTML = longBreakTime;
+document.getElementById("longBreakUp").addEventListener("click", () => {
+  longBreakTime++;
+  document.getElementById("longBreakTime").innerHTML = longBreakTime;
   if (pomodoroState === 2) {
     timerReset();
   }
 });
-document.getElementById('longBreakDown').addEventListener('click', () => {
+document.getElementById("longBreakDown").addEventListener("click", () => {
   if (longBreakTime > 1) {
     longBreakTime--;
-  }    
-  document.getElementById('longBreakTime').innerHTML = longBreakTime;
+  }
+  document.getElementById("longBreakTime").innerHTML = longBreakTime;
   if (pomodoroState === 2) {
     timerReset();
   }
 });
 
-document.getElementById('timerBlock').addEventListener('click', () => {
-  if (!timerRunning) {
-    if (timerMinutes <= 0 && timerSeconds <= 0) {      
-    }
-    else {
-      timerRunning = true;
-      secondCountDown();      
-    }
-  }
-  else if (timerRunning) {
-    stopCountDown();
-  }
-  else {
-    console.log('Error. How did this happen?');
-  }
-});
+document.getElementById("timerBlock").addEventListener("click", timerPress);
 
-document.getElementById('reset').addEventListener('click', () => {
+document.body.onkeydown = function(event) {
+  if (event.keyCode == 32) {
+    timerPress();
+  }
+};
+
+function timerPress() {
+  if (!timerRunning) {
+    if (timerMinutes <= 0 && timerSeconds <= 0) {
+    } else {
+      timerRunning = true;
+      secondCountDown();
+    }
+  } else if (timerRunning) {
+    stopCountDown();
+  } else {
+    console.log("Error. How did this happen?");
+  }
+}
+
+document.getElementById("reset").addEventListener("click", () => {
   timerReset();
-  
 });
 
 function secondCountDown() {
-  document.getElementById('timerDisplay').innerHTML = timerMinutes+':'+setTwoDigits(timerSeconds);
+  document.getElementById("timerDisplay").innerHTML =
+    timerMinutes + ":" + setTwoDigits(timerSeconds);
   timerSeconds--;
   if (timerSeconds < 0) {
     timerSeconds = 59;
@@ -97,56 +103,65 @@ function secondCountDown() {
   }
   if (timerMinutes < 0) {
     setNextPomodoroState();
-  }
-  else {
+  } else {
     timer = setTimeout(() => {
       secondCountDown();
     }, 1000);
-  }  
+  }
 }
 
 function stopCountDown() {
-    clearTimeout(timer);
-    timerRunning = false;  
+  clearTimeout(timer);
+  timerRunning = false;
 }
 
 function setTwoDigits(num) {
-  let newStr = '00'+num;
+  let newStr = "00" + num;
   return newStr.slice(-2);
 }
 
-function setNextPomodoroState() {	
+function setNextPomodoroState() {
   audio.play();
   if (pomodoroState === 0) {
     completedPomodoros++;
     pomoTimeSpent += sessionTime;
     totalTimeSpent += sessionTime;
-    if (completedPomodoros%4 === 0) {
+    if (completedPomodoros % 4 === 0) {
       pomodoroState = 2;
-      document.getElementById('stateDisplay').innerHTML = 'Long break<br/>'+completedPomodoros+' pomodoros completed';
-    }
-    else {
+      document.getElementById("stateDisplay").innerHTML =
+        "Long break<br/>" + completedPomodoros + " pomodoros completed";
+    } else {
       pomodoroState = 1;
-      document.getElementById('stateDisplay').innerHTML = 'Short break<br/>'+completedPomodoros+' pomodoros completed';
+      document.getElementById("stateDisplay").innerHTML =
+        "Short break<br/>" + completedPomodoros + " pomodoros completed";
     }
-  }
-  else {
+  } else {
     if (pomodoroState === 1) {
       totalTimeSpent += breakTime;
-    }
-    else if (pomodoroState === 2) {
+    } else if (pomodoroState === 2) {
       totalTimeSpent += longBreakTime;
     }
-    pomodoroState = 0;    
-    document.getElementById('stateDisplay').innerHTML = 'Session<br/>'+completedPomodoros+' pomodoros completed';
+    pomodoroState = 0;
+    document.getElementById("stateDisplay").innerHTML =
+      "Session<br/>" + completedPomodoros + " pomodoros completed";
   }
-  pomoTimeSpentHours = Math.floor(pomoTimeSpent/60);
-  pomoTimeSpentMins = pomoTimeSpent%60;
-  totalTimeSpentHours = Math.floor(totalTimeSpent/60);
-  totalTimeSpentMins = totalTimeSpent%60;
-  document.getElementById('info').innerHTML = 'You have spent '+pomoTimeSpentHours+' hours and '+pomoTimeSpentMins+' minutes immersed in your task.'
-  document.getElementById('info').innerHTML += '<br/>And a total of '+totalTimeSpentHours+' hours and '+totalTimeSpentMins+' minutes using the Pomodoro Technique.'
-  timerReset();  
+  pomoTimeSpentHours = Math.floor(pomoTimeSpent / 60);
+  pomoTimeSpentMins = pomoTimeSpent % 60;
+  totalTimeSpentHours = Math.floor(totalTimeSpent / 60);
+  totalTimeSpentMins = totalTimeSpent % 60;
+  document.getElementById("info").innerHTML =
+    "You have spent " +
+    pomoTimeSpentHours +
+    " hours and " +
+    pomoTimeSpentMins +
+    " minutes immersed in your task.";
+  document.getElementById("info").innerHTML +=
+    "<br/>And a total of " +
+    totalTimeSpentHours +
+    " hours and " +
+    totalTimeSpentMins +
+    " minutes using the Pomodoro Technique.";
+  timerReset();
 }
 
 function timerReset() {
@@ -164,9 +179,9 @@ function timerReset() {
       timerMinutes = longBreakTime;
       break;
     default:
-      alert('Unknown pomodoro state');
-      break;      
+      alert("Unknown pomodoro state");
+      break;
   }
   timerSeconds = 0;
-  document.getElementById('timerDisplay').innerHTML = timerMinutes;
+  document.getElementById("timerDisplay").innerHTML = timerMinutes;
 }
